2e2d5fb067a25271ef75203bee441c3dfea7e922
aacohen <arielaades@gmail.com>
2016-10-06 18:59:53 -0400

    completed lab

--- Animals/AnimalListViewController.swift
+++ Animals/AnimalListViewController.swift
@@ -9,4 +9,53 @@
 import UIKit
 
 class AnimalListViewController: UITableViewController {
+    
+    var animalArray = ["Dog", "Cat", "Mouse", "Hamster", "Bunny", "Panda", "Lion", "Pig", "Frog", "Octopus"]
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return animalArray.count
+    }
+    // this function should return a UITableViewCell
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "AnimalCell", for: indexPath)
+        
+        let selectedAnimal = animalArray[indexPath.row]
+        
+//        cell.largeLabelOutlet.text =
+//        cell.smallLabelOutlet.text = selectedAnimal
+        
+        cell.textLabel?.text = selectedAnimal
+        
+        
+        return cell
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "AnimalDetailView" {
+           
+            var dest = segue.destination as! AnimalViewController
+            var selectedAnimal1: String = ""
+            
+            if let row = tableView.indexPathForSelectedRow?.row {
+                selectedAnimal1 = animalArray[row]
+            }
+            
+            dest.animal = selectedAnimal1
+        }
+    }
+
+
+    
+
+
+    
+    
+    
+    
+    
+    
 }
--- Animals/AnimalViewController.swift
+++ Animals/AnimalViewController.swift
@@ -9,4 +9,48 @@
 import UIKit
 
 class AnimalViewController: UIViewController {
+    @IBOutlet weak var largeLabelOutlet: UILabel!
+    
+    @IBOutlet weak var smallLabelOutlet: UILabel!
+    var animal: String?
+    
+    override func viewWillAppear(_ animated: Bool) {
+        if let animal = animal {
+        smallLabelOutlet.text = animal
+        largeLabelOutlet.text = animalEmoji(animal: animal)
+        print("This is the value of animal \(animal)")
+        }
+    }
+    
+    func animalEmoji(animal: String) -> String {
+        
+        switch animal{
+            
+        case "Dog":
+            return "🐶"
+        case "Cat":
+            return "🐱"
+        case "Mouse":
+            return "🐭"
+        case "Hamster":
+            return "🐹"
+        case "Bunny":
+            return "🐰"
+        case "Panda":
+            return "🐼"
+        case "Lion":
+            return "🦁"
+        case "Pig":
+            return "🐷"
+        case "Frog":
+            return "🐸"
+        case "Octopus":
+            return "🐙"
+        default:
+            return ""
+        }
+    }
+    
+    
+    
 }
